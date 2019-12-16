5961526c0447886e2de8233c07e8911e5175bb99
Rafal <Rafal.Wywrot@onet.pl>
2019-12-16 21:03:46 +0100

    init project with unit tests and invidual authentication

---
+++ LearningService/LearningService.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningService.WebApplication.Startup))]
+namespace LearningService.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
