5adc9c97b8f3c0e9f2be4e34e08fec2e1eccdd54
Kinga <Kinga.Golojuch@gmail.com>
2020-02-03 22:09:06 +0100

    user service

---
+++ LearningService/LearningService.Domain/Services/Concrete/UserService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using LearningService.DAO.Repositories.Abstract;
+using LearningService.Domain.ModelsDTO;
+using LearningService.Domain.Services.Abstract;
+using System.Collections.Generic;
+
+namespace LearningService.Domain.Services.Concrete
+{
+    public class UserService : IUserService
+    {
+        private readonly ITestRepository _testRepository;
+
+        public UserService(ITestRepository testRepository)
+        {
+            this._testRepository = testRepository;
+        }
+
+        public IEnumerable<TestDTO> Get()
+        {
+            var entities = _testRepository.Get();
+            return Mapper.Map<IEnumerable<TestDTO>>(entities);
+        }
+
+        public TestDTO Get(int id)
+        {
+            var entity = _testRepository.GetById(id);
+            return new TestDTO
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Accepted = entity.Accepted
+            };
+        }
+    }
+}
