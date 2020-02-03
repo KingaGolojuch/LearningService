a68e74a52154ce4a0819c4d318a8623544450451
Kinga <Kinga.Golojuch@gmail.com>
2020-02-03 23:57:17 +0100

    lesson type

---
+++ LearningService/LearningService.DAO/Mappings/LessonTypeMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+using LearningService.DAO.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningService.DAO.Mappings
+{
+    public class LessonTypeMap : ClassMap<LessonType>
+    {
+        public LessonTypeMap()
+        {
+            Table("LessonType");
+
+            Id(x => x.Id).GeneratedBy.Assigned();
+            Map(x => x.Name, "Name");
+        }
+    }
+}
