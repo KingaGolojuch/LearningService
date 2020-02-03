0a1c4acfee7afaac4e6ebd6771b95ef4afc22144
Kinga <Kinga.Golojuch@gmail.com>
2020-02-03 22:08:43 +0100

    user repositories

---
+++ LearningService/LearningService.DAO/Repositories/Abstract/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using LearningService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningService.DAO.Repositories.Abstract
+{
+    public interface IUserRepository
+    {
+        ApplicationUser Get(int id);
+    }
+}
---
+++ LearningService/LearningService.DAO/Repositories/Concrete/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using LearningService.DAO.Helpers;
+using LearningService.DAO.Repositories.Abstract;
+using LearningService.Helpers;
+
+namespace LearningService.DAO.Repositories.Concrete
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserRepository(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public ApplicationUser Get(int id)
+        {
+            return _unitOfWork.Session.Get<ApplicationUser>(id);
+        }
+    }
+}
