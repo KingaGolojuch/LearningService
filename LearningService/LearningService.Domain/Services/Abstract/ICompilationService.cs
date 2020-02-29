cd89469594e335a3f02879c55813edfe33c3528a
Kinga <Kinga.Golojuch@gmail.com>
2020-02-29 14:39:24 +0100

    add empty compilation service

---
+++ LearningService/LearningService.Domain/Services/Abstract/ICompilationService.cs
@@ -0,0 +1,10 @@
+﻿using LearningService.Domain.Enums;
+using LearningService.Domain.ModelsDTO;
+using System.Collections.Generic;
+
+namespace LearningService.Domain.Services.Abstract
+{
+    public interface ICompilationService
+    {
+    }
+}
---
+++ LearningService/LearningService.Domain/Services/Concrete/CompilationService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using LearningService.DAO.Entities;
+using LearningService.DAO.Repositories.Abstract;
+using LearningService.Domain.Enums;
+using LearningService.Domain.Exceptions;
+using LearningService.Domain.ModelsDTO;
+using LearningService.Domain.Services.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningService.Domain.Services.Concrete
+{
+    public class CompilationService : ICompilationService
+    {
+        private readonly ICourseRepository _courseRepository;
+        private readonly ILessonRepository _lessonRepository;
+        private readonly IUserRepository _userRepository;
+
+        public CompilationService(
+            ICourseRepository courseRepository,
+            ILessonRepository lessonRepository,
+            IUserRepository userRepository)
+        {
+            _courseRepository = courseRepository;
+            _lessonRepository = lessonRepository;
+            _userRepository = userRepository;
+        }
+
+    }
+}
