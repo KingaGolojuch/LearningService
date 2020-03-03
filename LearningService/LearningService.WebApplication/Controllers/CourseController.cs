5ca7d746fa0cef583a93880a98e3239eefd1b46f
Kinga <Kinga.Golojuch@gmail.com>
2020-03-03 23:27:50 +0100

    sending question to course owner

--- LearningService/LearningService.WebApplication/Controllers/CourseController.cs
+++ LearningService/LearningService.WebApplication/Controllers/CourseController.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using LearningService.Domain.ModelsDTO;
 using LearningService.Domain.Services.Abstract;
+using LearningService.WebApplication.Helpers;
 using LearningService.WebApplication.Models.Course;
 using LearningService.WebApplication.Models.Lesson;
 using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace LearningService.WebApplication.Controllers
@@ -16,15 +18,18 @@ namespace LearningService.WebApplication.Controllers
         private readonly ICourseService _courseService;
         private readonly ILessonService _lessonService;
         private readonly IUserService _userService;
+        private ApplicationUserManager _userManager;
 
         public CourseController(
             ICourseService courseService,
             ILessonService lessonService,
-            IUserService userService)
+            IUserService userService,
+            ApplicationUserManager userManager)
         {
             _courseService = courseService;
             _lessonService = lessonService;
             _userService = userService;
+            _userManager = userManager;
         }
         // GET: Test
         public ActionResult Index()
@@ -76,6 +81,29 @@ namespace LearningService.WebApplication.Controllers
         {
             _userService.AddCourseSubscription(GetUserId, courseId);
             return RedirectToAction("CourseOverwiew", new { courseId });
+        }
+
+        public ActionResult AskQuestion(int courseId)
+        {
+            var model = new CourseMailViewModel
+            {
+                CourseId = courseId
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AskQuestion(CourseMailViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var course = _courseService.Get(model.CourseId);
+            var courseOwenr = _userManager.GetEmail(course.UserId);
+            var userLoggedIn = _userManager.GetEmail(GetUserId);
+            var mailContent = $"Użytkownik o mailu {userLoggedIn} zadał ci pytanie: {model.MailContent}";
+            await MailNotification.SendEmail(courseOwenr, $"Otrzymałeś wiadomość do kursu: {course.Name}", mailContent);
+            return RedirectToAction("CourseOverwiew", new { model.CourseId });
         }
     }
 }
